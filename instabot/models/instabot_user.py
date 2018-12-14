63fea40cb75ae22f196b712b6f259a8d5ffa9d39
Evgeny Kemerov <eskemerov@gmail.com>
2018-12-14 16:08:44 +0300

    Rename User to InstabotUser

--- instabot/models/instabot_user.py
+++ instabot/models/instabot_user.py
@@ -3,7 +3,10 @@ from peewee import CharField, IntegerField
 from .base import BaseModel
 
 
-class User(BaseModel):
+class InstabotUser(BaseModel):
+    """
+    Instabot user is used for authentication and using Instabot
+    """
     user_id = IntegerField(unique=True)
     username = CharField(unique=True)
     password = CharField()
