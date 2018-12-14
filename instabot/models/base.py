afafba11305792a761dd87b522e1e6eccc430521
Evgeny Kemerov <eskemerov@gmail.com>
2018-12-14 16:08:25 +0300

    Fix updated_at in BaseModel

--- instabot/models/base.py
+++ instabot/models/base.py
@@ -9,5 +9,10 @@ class BaseModel(Model):
     created_at = DateTimeField(default=datetime.now)
     updated_at = DateTimeField()
 
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now()
+
+        super(BaseModel, self).save(*args, **kwargs)
+
     class Meta:
         database = db
